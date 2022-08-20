4a7466ee3160631f6578f62d18f7df9ab4e11a70
kevin <kevinchannon@gmail.com>
2022-08-20 09:03:02 +0100

    WIP

--- test/string/splitTests.cpp
+++ test/string/splitTests.cpp
@@ -10,36 +10,52 @@
 using namespace std::string_literals;
 using namespace wite;
 
-TEST_CASE("Split string defaults to using space as a delimiter", "[string]") {
-  const auto words = string::split("one two three");
-  const auto expected = std::vector<std::string_view>{"one", "two", "three"};
-  
-  REQUIRE(expected.size() == words.size());
-  REQUIRE(std::equal(expected.begin(), expected.end(), words.begin()));
-}
-
 TEST_CASE("Split string tests", "[string]") {
+  SECTION("Defaults to using space as a delimiter", "[string]") {
+    const auto words = string::split("one two three");
+    const auto expected = std::vector<std::string_view>{"one", "two", "three"};
+
+    REQUIRE(expected.size() == words.size());
+    REQUIRE(std::equal(expected.begin(), expected.end(), words.begin()));
+  }
+
   struct TestParams {
     std::string name;
     std::string toSplit;
     char delimiter;
+    string::split_behaviour behaviour;
     std::vector<std::string_view> expected;
   };
 
   const auto testParams = GENERATE(
-    TestParams{"Split string on space", "One small step for man...", ' ', {"One", "small", "step", "for", "man..."}},
-    TestParams{"Split string on non-space", "1,2,3,4,5", ',', {"1", "2", "3", "4", "5"}},
-    TestParams{"Split string without any delimiters in it", "1 2 3 4 5", ',', {"1 2 3 4 5"}},
-    TestParams{"Split string containing only delimiters works", "  ", ' ', {"", "", ""}},
-    TestParams{"Split string starting with a delimiter works", " 1", ' ', {"", "1"}},
-    TestParams{"Split string ending with a delimiter works", "1 ", ' ', {"1", ""}},
-    TestParams{"Split string ending with a delimiter works", "1 ", ' ', {"1", ""}}
+    TestParams{"Split string on space",
+        "One small step for man...",' ', string::split_behaviour::drop_empty, {"One", "small", "step", "for", "man..."}},
+    TestParams{"Split string on non-space",
+        "1,2,3,4,5", ',', string::split_behaviour::drop_empty, {"1", "2", "3", "4", "5"}},
+    TestParams{"Split string on space, keeping empty items",
+        "1,2,,3,4,5", ',', string::split_behaviour::keep_empty, {"1", "2", "3", "4", "5"}},,
+    TestParams{"Split string on space, dropping empty items",
+        "1,2,,3,4,5", ',', string::split_behaviour::drop_empty, {"1", "2", "3", "4", "5"}},
+    TestParams{"Split string without any delimiters in it",
+        "1 2 3 4 5", ',', string::split_behaviour::drop_empty, {"1 2 3 4 5"}},
+    TestParams{"Split string containing only delimiters works, keeping empty items",
+        "  ", ' ', string::split_behaviour::keep_empty, {"", "", ""}},
+    TestParams{"Split string containing only delimiters works, dropping empty items",
+        "  ", ' ', string::split_behaviour::drop_empty, {}},
+    TestParams{"Split string starting with a delimiter works, keeping empty items",
+        " 1", ' ', string::split_behaviour::keep_empty, {"", "1"}},
+    TestParams{"Split string starting with a delimiter works, dropping empty items",
+        " 1", ' ', string::split_behaviour::drop_empty, {"1"}},
+    TestParams{"Split string ending with a delimiter works, keeping empty items",
+        "1 ", ' ', string::split_behaviour::keep_empty, {"1", ""}},
+    TestParams{"Split string starting with a delimiter works, dropping empty items",
+        " 1", ' ', string::split_behaviour::drop_empty, {"1"}}
   );
 
   const auto words = string::split(testParams.toSplit, testParams.delimiter);
-  
+
   SECTION(testParams.name){
-      REQUIRE(testParams.expected.size() == words.size());
-      REQUIRE(std::equal(testParams.expected.begin(), testParams.expected.end(), words.begin()));
+    REQUIRE(testParams.expected.size() == words.size());
+    REQUIRE(std::equal(testParams.expected.begin(), testParams.expected.end(), words.begin()));
   }
 }
--- wite/string/split.hpp
+++ wite/string/split.hpp
@@ -3,6 +3,7 @@
 #include <string_view>
 #include <vector>
 #include <tuple>
+#include <cstdint>
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -10,23 +11,57 @@ namespace wite::string {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-inline std::tuple<std::string_view, std::string_view, bool> first_token(std::string_view str, char delimiter) {
-  const auto pos = std::find(str.begin(), str.end(), delimiter);
+enum class split_behaviour : uint8_t{
+  keep_empty,
+  drop_empty
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+namespace detail {
+  
+inline std::string_view::const_iterator advance_past_delimiter(
+  std::string_view::const_iterator begin,
+  const std::string_view::const_iterator end,
+  char delimiter,
+  split_behaviour behaviour)
+{
+  auto out = (begin == end ? begin : std::next(begin));
+  if (behaviour == split_behaviour::drop_empty) {
+    out = std::find_if(begin, end, [delimiter](auto c) { return c != delimiter; });
+  }
+  
+  return out;
+}
+  
+inline std::tuple<std::string_view, std::string_view, bool> first_token(
+  std::string_view str,
+  char delimiter,
+  split_behaviour behaviour)
+{
+  const auto token_end = std::find(str.begin(), str.end(), delimiter);
+  auto start_next = advance_past_delimiter(token_end, str.end(), delimiter, behaviour);
+  
   return {
-    {str.begin(), pos},
-    {pos == str.end() ? pos : std::next(pos), str.end()},
-    pos != str.end()
+    {str.begin(), token_end},
+    {start_next, str.end()},
+    token_end != str.end()
   };
+}
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-inline std::vector<std::string_view> split(std::string_view str, char delimiter = ' ') {
+inline std::vector<std::string_view> split(std::string_view str, char delimiter = ' ', split_behaviour behaviour=split_behaviour::drop_empty) {
   auto result = std::vector<std::string_view>{};
 
   while (true) {
-    auto [token, remainder, performed_a_split] = first_token(str, delimiter);
-    result.push_back(std::move(token));
+    auto [token, remainder, performed_a_split] = detail::first_token(str, delimiter, behaviour);
+    
+    if (split_behaviour::keep_empty == behaviour or (split_behaviour::drop_empty == behaviour and (not token.empty()))) {
+      result.push_back(std::move(token));
+    }
 
     if (not performed_a_split) {
       break;
