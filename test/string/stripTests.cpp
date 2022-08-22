6de0105187a4e75f76888bb11f7f7b75c81a1919
kevin <kevinchannon@gmail.com>
2022-08-22 13:56:45 +0100

    WIP

--- test/string/stripTests.cpp
+++ test/string/stripTests.cpp
@@ -9,7 +9,7 @@ using namespace wite;
 TEST_CASE("Strip strings", "[string]"){
   SECTION("Narrow strings") {
     SECTION("C-string with nothing to strip is not changed") {
-//      REQUIRE("Nothing to strip"s == string::strip("Nothing to strip"));
+      REQUIRE("Nothing to strip"s == string::strip("Nothing to strip"));
     }
     
     SECTION("String with nothing to strip is not changed") {
@@ -19,5 +19,62 @@ TEST_CASE("Strip strings", "[string]"){
     SECTION("String view with nothing to strip is not changed") {
       REQUIRE("Nothing to strip"s == string::strip(std::string_view{"Nothing to strip"}));
     }
+    
+    SECTION("Empty string") {
+      REQUIRE(""s == string::strip(""));
+    }
+    
+    SECTION("Only whitespace") {
+      const auto whitespace_str = GENERATE("   ", "\t\t\t", "\n\n\n", "\r\r\r", "\v\v\v");
+      REQUIRE(""s == string::strip(whitespace_str));
+      REQUIRE(""s == string::strip(std::string(whitespace_str)));
+      REQUIRE(""s == string::strip(std::string_view(whitespace_str)));
+    }
+    
+    SECTION("Leading and trailing white-space is removed") {
+      const auto [str, expected] = GENERATE(table<const char*, std::string>({
+        {" space ", "space"s},
+        {"\ttab\t", "tab"s},
+        {"\rcarriage return\r", "carriage return"}, 
+        {"\vvertical tab\v", "vertical tab"}
+      }));
+      
+      REQUIRE(expected == string::strip(str));
+    }
+  }
+  
+  SECTION("Wide strings") {
+    SECTION("C-string with nothing to strip is not changed") {
+      REQUIRE(L"Nothing to strip"s == string::strip(L"Nothing to strip"));
+    }
+    
+    SECTION("String with nothing to strip is not changed") {
+      REQUIRE(L"Nothing to strip"s == string::strip(L"Nothing to strip"s));
+    }
+    
+    SECTION("String view with nothing to strip is not changed") {
+      REQUIRE(L"Nothing to strip"s == string::strip(std::wstring_view{L"Nothing to strip"}));
+    }
+    
+    SECTION("Empty string") {
+      REQUIRE(L""s == string::strip(L""));
+    }
+    
+    SECTION("Only whitespace") {
+      const auto whitespace_str = GENERATE(L"   ", L"\t\t\t", L"\n\n\n", L"\r\r\r", L"\v\v\v");
+      
+      REQUIRE(L""s == string::strip(whitespace_str));
+      REQUIRE(L""s == string::strip(std::wstring(whitespace_str)));
+      REQUIRE(L""s == string::strip(std::wstring_view(whitespace_str)));
+    }
+    
+    const auto [str, expected] = GENERATE(table<const wchar_t*, std::wstring>({
+      {L" space ", L"space"s},
+      {L"\ttab\t", L"tab"s},
+      {L"\rcarriage return\r", L"carriage return"}, 
+      {L"\vvertical tab\v", L"vertical tab"}
+    }));
+    
+    REQUIRE(expected == string::strip(str));
   }
 }
--- wite/string/strip.hpp
+++ wite/string/strip.hpp
@@ -7,20 +7,22 @@
 #include <type_traits>
 
 namespace wite::string {
+
+///////////////////////////////////////////////////////////////////////////////
   
-template<typename Char_T>
-std::basic_string<Char_T> strip(std::basic_string_view<Char_T> str)
+template<typename String_T>
+std::basic_string<typename String_T::value_type> strip(const String_T& str)
 {
   return trim_left(trim_right(str));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-//template<typename Char_T>
-//std::basic_string<Char_T> strip(const Char_T* str)
-//{
-//  return trim_left(trim_right(std::basic_string_view<Char_T>{str}));
-//}
+template<typename Char_T>
+std::basic_string<Char_T> strip(const Char_T* str)
+{
+  return trim_left(trim_right(std::basic_string_view<Char_T>{str}));
+}
 
 ///////////////////////////////////////////////////////////////////////////////
   
