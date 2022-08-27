233443a92bb3f95d714a84f39f6316908e7fbd7d
kevin <kevinchannon@gmail.com>
2022-08-27 07:37:46 +0100

    Make test values a bit more sane

--- test/io/bytesBufferTests.cpp
+++ test/io/bytesBufferTests.cpp
@@ -11,29 +11,31 @@
 using namespace wite;
 
 TEST_CASE("Values from vector buffer", "[buffer_io]") {
-  const auto vec_buffer = std::vector<std::byte>{std::byte{0x78},
-                                                 std::byte{0x56},
-                                                 std::byte{0x34},
-                                                 std::byte{0x12},
+  const auto vec_buffer = std::vector<std::byte>{std::byte{0x67},
+                                                 std::byte{0x45},
+                                                 std::byte{0x23},
+                                                 std::byte{0x01},
                                                  std::byte{0xEF},
                                                  std::byte{0xCD},
                                                  std::byte{0xAB},
                                                  std::byte{0x89}};
 
   SECTION("Read int") {
-    REQUIRE(uint32_t(0x12345678) == io::buffers::read<uint32_t>(vec_buffer));
+    REQUIRE(uint32_t(0x01234567) == io::buffers::read<uint32_t>(vec_buffer));
   }
 
   SECTION("Read 2 shorts") {
-    REQUIRE(uint32_t(0x5678) == io::buffers::read<uint16_t>(vec_buffer));
-    REQUIRE(uint32_t(0x1234) == io::buffers::read<uint16_t>({std::next(vec_buffer.begin(), 2), vec_buffer.end()}));
+    REQUIRE(uint32_t(0x4567) == io::buffers::read<uint16_t>(vec_buffer));
+    REQUIRE(uint32_t(0x0123) == io::buffers::read<uint16_t>({std::next(vec_buffer.begin(), 2), vec_buffer.end()}));
   }
+
 }
 
 TEST_CASE("Byte streams", "[buffer_io]") {
   SECTION("Read int") {
-    std::stringstream stream("\x78\x56\x34\x12");
+    std::stringstream stream("\x67\x45\x23\x01\xEF\xCD\xAB\x89");
 
-    REQUIRE(uint32_t(0x12345678) == io::buffers::read<uint32_t>(stream));
+    REQUIRE(uint32_t(0x01234567) == io::buffers::read<uint32_t>(stream));
+    REQUIRE(uint32_t(0x89ABCDEF) == io::buffers::read<uint32_t>(stream));
   }
 }
