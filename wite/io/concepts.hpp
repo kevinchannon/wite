2cba868440a9e900f2ed576b7533b38f252011f5
kevin <kevinchannon@gmail.com>
2022-09-29 07:07:52 +0100

    Add requirement that value isn't a pointer to is_buffer_(readable|writable)

--- wite/io/concepts.hpp
+++ wite/io/concepts.hpp
@@ -17,12 +17,12 @@ _WITE_CONCEPT is_encoded = std::is_base_of_v<io::encoding, T>;
 
 #if _WITE_HAS_CONCEPTS
 template <typename T>
-_WITE_CONCEPT is_buffer_readable =
-    (not std::is_array_v<T>)and(common::is_pod_like<T> or (is_encoded<T> and common::is_pod_like<typename T::value_type>));
+_WITE_CONCEPT is_buffer_readable = (not(std::is_array_v<T> or std::is_pointer_v<T>)) and
+                                   (common::is_pod_like<T> or (is_encoded<T> and common::is_pod_like<typename T::value_type>));
 
 template <typename T>
-_WITE_CONCEPT is_buffer_writeable =
-    (not std::is_array_v<T>)and(common::is_pod_like<T> or (is_encoded<T> and common::is_pod_like<typename T::value_type>));
+_WITE_CONCEPT is_buffer_writeable = (not(std::is_array_v<T> or std::is_pointer_v<T>)) and
+                                    (common::is_pod_like<T> or (is_encoded<T> and common::is_pod_like<typename T::value_type>));
 #else  // ^^^ _WITE_HAS_CONCEPTS / vvv !_WITE_HAS_CONCEPTS
 namespace detail {
   template <typename T>
@@ -38,10 +38,10 @@ namespace detail {
 }  // namespace detail
 
 template <typename T>
-_WITE_CONCEPT is_buffer_readable = detail::_is_pod_like<T>();
+_WITE_CONCEPT is_buffer_readable = (not(std::is_array_v<T> or std::is_pointer_v<T>)) and detail::_is_pod_like<T>();
 
 template <typename T>
-_WITE_CONCEPT is_buffer_writeable = detail::_is_pod_like<T>();
+_WITE_CONCEPT is_buffer_writeable = (not(std::is_array_v<T> or std::is_pointer_v<T>)) and detail::_is_pod_like<T>();
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
