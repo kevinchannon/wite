a1e36d9357106eb12d781730b0ab6fada2c57532
kevin <kevinchannon@gmail.com>
2022-10-04 07:11:51 +0100

    Move fragment_string into the root wite namespace; add some tests for wide-strings

--- test/string/fragmentStringTest.cpp
+++ test/string/fragmentStringTest.cpp
@@ -7,32 +7,64 @@ using namespace wite;
 TEST_CASE("fragment_string tests", "[string]") {
   SECTION("construct") {
     SECTION("from a single C-string") {
-      const auto fs = string::fragment_string{"hello!"};
+      SECTION("narrow chars") {
+        const auto fs = fragment_string{"hello!"};
 
-      REQUIRE(6 == fs.length());
-      REQUIRE(std::string{"hello!"} == fs.to_str());
+        REQUIRE(6 == fs.length());
+        REQUIRE(std::string{"hello!"} == fs.to_str());
+      }
+
+      SECTION("wide chars") {
+        const auto fs = fragment_wstring{L"hello!"};
+
+        REQUIRE(6 == fs.length());
+        REQUIRE(std::wstring{L"hello!"} == fs.to_str());
+      }
     }
   }
 
   SECTION("concatenate") {
-    const auto fs_1 = string::fragment_string{"me "};
-    REQUIRE(std::string{"me "} == fs_1.to_str());
+    SECTION("narrow chars") {
+      const auto fs_1 = fragment_string{"me "};
+      REQUIRE(std::string{"me "} == fs_1.to_str());
 
-    const auto fs_2 = fs_1 + "thinks ";
-    REQUIRE(std::string{"me thinks "} == fs_2.to_str());
+      const auto fs_2 = fs_1 + "thinks ";
+      REQUIRE(std::string{"me thinks "} == fs_2.to_str());
 
-    const auto fs_3 = string::fragment_string{"it "};
-    REQUIRE(std::string{"it "} == fs_3.to_str());
+      const auto fs_3 = fragment_string{"it "};
+      REQUIRE(std::string{"it "} == fs_3.to_str());
 
-    const auto fs_4 = fs_2 + fs_3;
-    REQUIRE(std::string{"me thinks it "} == fs_4.to_str());
+      const auto fs_4 = fs_2 + fs_3;
+      REQUIRE(std::string{"me thinks it "} == fs_4.to_str());
 
-    const auto fs_5 = fs_4 + "is " + "a " + "weasle";
-    REQUIRE(std::string{"me thinks it is a weasle"} == fs_5.to_str());
+      const auto fs_5 = fs_4 + "is " + "a " + "weasle";
+      REQUIRE(std::string{"me thinks it is a weasle"} == fs_5.to_str());
+    }
+
+    SECTION("wide chars") {
+      const auto fs_1 = fragment_wstring{L"me "};
+      REQUIRE(std::wstring{L"me "} == fs_1.to_str());
+
+      const auto fs_2 = fs_1 + L"thinks ";
+      REQUIRE(std::wstring{L"me thinks "} == fs_2.to_str());
+
+      const auto fs_3 = fragment_wstring{L"it "};
+      REQUIRE(std::wstring{L"it "} == fs_3.to_str());
+
+      const auto fs_4 = fs_2 + fs_3;
+      REQUIRE(std::wstring{L"me thinks it "} == fs_4.to_str());
+
+      const auto fs_5 = fs_4 + L"is " + L"a " + L"weasle";
+      REQUIRE(std::wstring{L"me thinks it is a weasle"} == fs_5.to_str());
+    }
   }
 
   SECTION("length and size match") {
-    const auto fs = string::fragment_string{"Lorem ipsum dolor sit"};
+    const auto fs = fragment_string{"Lorem ipsum dolor sit"};
     REQUIRE(fs.length() == fs.size());
   }
+}
+
+TEST_CASE("fragment_string iterator", "[string]") {
+
 }
