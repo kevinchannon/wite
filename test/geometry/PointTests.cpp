9a47e389f3992fde1230a700ee55866979faa342
kevin <kevinchannon@gmail.com>
2022-10-23 09:31:18 +0100

    Add initializer-list construction for point template

--- test/geometry/PointTests.cpp
+++ test/geometry/PointTests.cpp
@@ -22,4 +22,27 @@ TEST_CASE("Point Tests", "[geometry]") {
       REQUIRE(size_t{4} == point_4d{}.dimensions());
     }
   }
+
+  SECTION("construct from initializer list") {
+    const auto p = point_4d{1.1, 2.2, 3.3, 4.4}; 
+
+    REQUIRE(1.1 == p.get<0>());
+    REQUIRE(2.2 == p.get<1>());
+    REQUIRE(3.3 == p.get<2>());
+    REQUIRE(4.4 == p.get<3>());
+  }
+
+  SECTION("get()") {
+    SECTION("values are default-constructed") {
+      REQUIRE(WITE_DEFAULT_POINT_TYPE{} == point_3d{}.get<0>());
+      REQUIRE(WITE_DEFAULT_POINT_TYPE{} == point_3d{}.get<1>());
+      REQUIRE(WITE_DEFAULT_POINT_TYPE{} == point_3d{}.get<2>());
+    }
+
+    SECTION("returns the expected value for each dimension") {
+      REQUIRE(1.23 == point_3d{1.23, 3.14, 2.72}.get<0>());
+      REQUIRE(3.14 == point_3d{1.23, 3.14, 2.72}.get<1>());
+      REQUIRE(2.72 == point_3d{1.23, 3.14, 2.72}.get<2>());
+    }
+  }
 }
--- wite/geometry/point.hpp
+++ wite/geometry/point.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <wite/common/constructor_macros.hpp>
+
+#include <algorithm>
 #include <array>
 #include <cstdint>
 
@@ -14,11 +17,21 @@ class point {
   using _storage_type = std::array<Value_T, DIMENSION_COUNT>;
 
  public:
+  using value_type = Value_T;
+  using size_type  = typename _storage_type::size_type;
 
-   using value_type = Value_T;
-   using size_type = typename _storage_type::size_type;
+  WITE_DEFAULT_CONSTRUCTORS(point);
+
+  constexpr point(_storage_type val) : _value{std::move(val)} {}
+
+  constexpr point(std::initializer_list<value_type> init) { std::ranges::copy(init, _value.begin()); }
 
   [[nodiscard]] constexpr size_type dimensions() const noexcept { return _value.size(); }
+
+  template <size_t DIM>
+  [[nodiscard]] constexpr const value_type& get() const noexcept {
+    return _value[DIM];
+  }
 
  private:
   _storage_type _value;
@@ -33,4 +46,4 @@ using point_3d = point<3, Value_T>;
 template <typename Value_T = WITE_DEFAULT_POINT_TYPE>
 using point_4d = point<4, Value_T>;
 
-}
+}  // namespace wite::geometry
