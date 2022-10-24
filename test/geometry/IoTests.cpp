a25ab3c6720807fc8e165602cd90bf9cc079407e
kevin <kevinchannon@gmail.com>
2022-10-24 19:39:20 +0100

    Output a geometry::point to a wide ostream

--- test/geometry/IoTests.cpp
+++ test/geometry/IoTests.cpp
@@ -7,8 +7,17 @@
 using namespace wite::geometry;
 
 TEST_CASE("Write point to ostream", "[geometry]") {
-  std::stringstream ss;
-  ss << point_2d{1.2, 3.4};
+  SECTION("narrow characters") {
+    std::stringstream ss;
+    ss << point_2d{1.2, 3.4};
 
-  REQUIRE("[ 1.2, 3.4 ]" == ss.str());
+    REQUIRE("[ 1.2, 3.4 ]" == ss.str());
+  }
+
+  SECTION("wide characters") {
+    std::wstringstream ss;
+    ss << point_2d{1.2, 3.4};
+
+    REQUIRE(L"[ 1.2, 3.4 ]" == ss.str());
+  }
 }
--- wite/geometry/io.hpp
+++ wite/geometry/io.hpp
@@ -16,3 +16,16 @@ std::ostream& operator<<(std::ostream& os, const wite::geometry::point<N_DIM, Va
 
   return os;
 }
+
+template <size_t N_DIM, typename Value_T>
+std::wostream& operator<<(std::wostream& os, const wite::geometry::point<N_DIM, Value_T>& p) {
+  os << "[ " << p[0];
+
+  for (auto i = 1u; i < p.dimensions(); ++i) {
+    os << ", " << p[i];
+  }
+
+  os << " ]";
+
+  return os;
+}
