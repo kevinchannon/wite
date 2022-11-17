227d3a684318b14afba9999512040d42bfd5f81b
kevin <kevinchannon@gmail.com>
2022-11-17 07:27:50 +0000

    make_vector can reserve some space in the vector

--- test/collections/makeVectorTests.cpp
+++ test/collections/makeVectorTests.cpp
@@ -1,4 +1,4 @@
-#include <wite/collections/make_Vector.hpp>
+#include <wite/collections/make_vector.hpp>
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
@@ -6,5 +6,9 @@
 TEST_CASE("make_vector tests") {
   SECTION("returns empty vector when called without arguments") {
     REQUIRE(wite::make_vector<double>().empty());
+  }
+
+  SECTION("reserve adapter creates vector with expected capacity") {
+    REQUIRE(10 == wite::make_vector<int>(wite::reserve{10}).capacity());
   }
 }
--- wite/collections/make_vector.hpp
+++ wite/collections/make_vector.hpp
@@ -6,9 +6,16 @@
 
 namespace wite {
 
+struct reserve {
+  size_t value{};
+};
+
 template<typename T>
-_WITE_NODISCARD std::vector<T> make_vector() {
-  return {};
+_WITE_NODISCARD std::vector<T> make_vector(reserve reserve_amount = reserve{}) {
+  auto out = std::vector<T>{};
+  out.reserve(reserve_amount.value);
+
+  return out;
 }
 
 }  // namespace wite
