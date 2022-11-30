48809f708b31c99feb1562da699cf06e9f9eaec1
Kevin Channon <kevinchannon@gmail.com>
2022-11-30 07:47:37 +0000

    Add the main bulk of functionality examples to identifiable_item_collection example code

--- examples/collections/identifiable_item_collection.cpp
+++ examples/collections/identifiable_item_collection.cpp
@@ -1,9 +1,11 @@
 #include <wite/collections/identifiable_item_collection.hpp>
 #include <wite/core/id.hpp>
+#include <wite/core/io.hpp>
 
 #include <algorithm>
 #include <cassert>
 #include <cstdint>
+#include <iostream>
 #include <vector>
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -85,6 +87,66 @@ int main() {
 
   populate(sensors);
   populate(servos);
+
+  //
+  // Check the size of the collections
+  //
+  std::cout << "Added " << sensors.size() << " sensors" << std::endl;
+  std::cout << "Added " << servos.size() << " servos" << std::endl;
+
+  //
+  // Check that particular items are present
+  //
+  const auto id_1 = Sensor::id_type{2};
+  const auto id_2 = Servo::id_type{3};
+
+  // Compilation will fail if we mix up these (poorly named) IDs. So, you can't write something like `sensors.contains(id_2);`
+  // because it won't compile.
+  if (not sensors.contains(id_1) or not servos.contains(id_2)) {
+    std::cerr << "Failed to find necessary sensor or servo" << std::endl;
+    return 1;
+  }
+
+  //
+  // Get using the ID. Again, the IDs can't be confused in these calls, or compilation will fail
+  //
+  const auto& sensor = sensors.at(id_1);
+  const auto& servo  = servos.at(id_2);
+
+  //
+  // NOTE: stream insertion of id types is possible, but you need to #include <wite/core/io.hpp> to get it.
+  //
+  std::cout << "Have sensor with ID " << sensor.id() << std::endl;
+  std::cout << "Have servo with ID " << servo.id() << std::endl;
+  //
+  // Loop over a collection.
+  //
+  // COMING SOON! Iterators, so that you don't have to type this safe, but verbose, stuff :)
+  for (auto i = SensorCollection::index_type{0}; i < SensorCollection::index_type{sensors.size()}; ++i) {
+    const auto& s =
+        sensors.at(i);  // This is an overload of at() that takes an index value. Like the id values, the indices are typed to the
+                        // container that they index into, so you can't accidentally use an index intended for use in the servo
+                        // collection to get an object from the sensor collection; it won't compile.
+    std::cout << "Sensor " << s.id() << ", value: " << s.value << std::endl;
+  }
+
+  //
+  // Remove items from a collection
+  //
+  sensors.erase(id_1);  // Typed-ID here means that it's not possible to accidentally put `id_2` here, or compilation will fail.
+
+  // Erase multiple elements (returns an array of boolean values indicating whether the item was erased, or not.
+  // `result` <-- [true, false], because we already erased id_1 from the collection.
+  const auto result = sensors.erase(Sensor::id_type{3}, id_1);
+
+  // To remove an item, but keep hold of it locally, then an item can be excised from the collection. NOTE: this is not called
+  // `extract`, which would seem to mirror standard library containers more closely. However, extract has some specific behaviours
+  // and uses that excise does not really implement at all, so it seems safer to give it a different name.
+  auto local_servo = servos.excise(id_2);
+
+  // Clear everything, if you need to do that, for some reason.
+  servos.clear();
+  
 
   return 0;
 }
